dceda41bae321aee11b2b183b5378be461a589ff
John Liu <john.liu@mongodb.com>
2021-04-13 10:48:23 -0400

    selection

--- src/Timechart.tsx
+++ src/Timechart.tsx
@@ -1,3 +1,4 @@
+import { Option, Select } from "@leafygreen-ui/select";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import {
@@ -26,54 +27,119 @@ interface CollectionStats {
   size_indices: number;
   size_total: number;
 
-  size_total_kb: number;
-  size_data_kb: number;
+  temp_display_metric: number;
 }
+
+const toKbyte = (size) => size / 1024;
+const noChange = (input) => input;
+
+type Metric = keyof CollectionStats;
+
+const metrics = {
+  num_docs: {
+    Label: "Number of Documents",
+    Unit: "Number",
+    Conversion: noChange,
+  },
+  num_indices: {
+    Label: "Number of Indices",
+    Unit: "Number",
+    Conversion: noChange,
+  },
+  size_data: {
+    Label: "Data Size (uncompressed)",
+    Unit: "Size (kb)",
+    Conversion: toKbyte,
+  },
+  size_storage: {
+    Label: "Data Storage Size",
+    Unit: "Size (kb)",
+    Conversion: toKbyte,
+  },
+  size_indices: {
+    Label: "Index Size",
+    Unit: "Size (kb)",
+    Conversion: toKbyte,
+  },
+  size_total: {
+    Label: "Total Size",
+    Unit: "Size (kb)",
+    Conversion: toKbyte,
+  },
+};
 
 interface Props {}
 
 export const Timechart: React.FC<Props> = () => {
   let [snapshots, setSnapshots] = useState<Snapshot[]>([]);
-  useEffect(() => {
+  let [metric, setMetric] = useState<Metric>("size_data");
+
+  const getData = () => {
     fetch("http://localhost:3001/snapshots")
       .then((res) => res.json())
       .then((snapshots) => {
         setSnapshots(snapshots);
         console.log(snapshots);
       });
+  };
+
+  const metricOptions = () => {
+    let options = [];
+    for (let metric in metrics) {
+      console.log(metric);
+      options.push(<Option value={metric}>{metrics[metric].Label}</Option>);
+    }
+    return options;
+  };
+
+  useEffect(() => {
+    getData();
+    const interval = setInterval(getData, 10000);
+    return () => clearInterval(interval);
   }, []);
 
   snapshots = snapshots.map((s) => {
-    s.stats.size_total_kb = toKbyte(s.stats.size_total);
-    s.stats.size_data_kb = toKbyte(s.stats.size_data);
+    s.stats.temp_display_metric = metrics[metric].Conversion(s.stats[metric]);
     return s;
   });
 
   return (
-    <LineChart
-      width={1500}
-      height={800}
-      data={snapshots}
-      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-    >
-      <Line type="monotone" dataKey="stats.size_data_kb" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis
-        dataKey="gathered_time_unix"
-        tickFormatter={(unixTime) => moment.unix(unixTime).format("HH:mm:ss")}
-        type="number"
-        domain={["auto", "auto"]}
-      />
-      <YAxis
-        dataKey="stats.size_data_kb"
-        type="number"
-        domain={["auto", "auto"]}
-        label="Size (kb)"
-        width={200}
-      />
-      <Tooltip />
-    </LineChart>
+    <div>
+      <Select
+        label="Metric"
+        value={metric}
+        onChange={(val: Metric) => setMetric(val)}
+      >
+        {metricOptions()}
+      </Select>
+      <LineChart
+        width={1500}
+        height={800}
+        data={snapshots}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line
+          type="monotone"
+          dataKey="stats.temp_display_metric"
+          isAnimationActive={false}
+          stroke="#8884d8"
+        />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis
+          dataKey="gathered_time_unix"
+          tickFormatter={(unixTime) => moment.unix(unixTime).format("HH:mm:ss")}
+          type="number"
+          domain={["auto", "auto"]}
+        />
+        <YAxis
+          dataKey="stats.temp_display_metric"
+          type="number"
+          domain={["auto", "auto"]}
+          label={metrics[metric].Unit}
+          width={200}
+        />
+        <Tooltip />
+      </LineChart>
+    </div>
   );
 };
-
-const toKbyte = (size) => size / 1024;
